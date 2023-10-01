23eecc0f46b133bb88e2ddcfa262397f67353994
Santiago E <santy.e@gmail.com>
2023-10-01 12:46:07 -0600

    Fix indentation with spaces (no tabs)

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-	const selectedAmenities = {};
-	$('input:checkbox').change(function () {
-		const amenityId = $(this).data('id');
-		const amenityName = $(this).data('name');
-
-        if ($(this).is(':checked')) {
-            selectedAmenities[amenityId] = amenityName;
-        } else {
-            delete selectedAmenities[amenityId];
-        }
-		const amenitiesList = Object.values(selectedAmenities).join(', ');
-        $('.amenities h4').text(amenitiesList);
-	});
+  const selectedAmenities = {};
+  $('input:checkbox').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    if ($(this).is(':checked')) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+    const amenitiesList = Object.values(selectedAmenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
 });
