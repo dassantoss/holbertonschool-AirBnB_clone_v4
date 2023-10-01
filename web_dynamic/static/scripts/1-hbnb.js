9cc888e259d19d1e79a5e7e790c6857909a0e724
Santiago E <santy.e@gmail.com>
2023-10-01 11:20:10 -0600

    Add checkbox functionality

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	const selectedAmenities = {};
+	$('input:checkbox').change(function () {
+		const amenityId = $(this).data('id');
+		const amenityName = $(this).data('name');
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+		const amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text(amenitiesList);
+	});
+});
