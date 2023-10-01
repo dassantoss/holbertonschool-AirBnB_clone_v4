1c669e43c4563f10e7c10a46d55168da36799137
Santiago E <santy.e@gmail.com>
2023-10-01 15:35:35 -0600

    Fixed API execution command for local development.

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,9 +12,11 @@ $(document).ready(function () {
     const amenitiesList = Object.values(selectedAmenities).join(', ');
     $('.amenities h4').text(amenitiesList);
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  $.get('http://localhost:5001/api/v1/status/', function (data) {
+    //console.log(data);
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
+      console.log('hola');
     } else {
       $('#api_status').removeClass('available');
     }
